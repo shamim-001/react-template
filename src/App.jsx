6973de8610de32dd9a9efcd3b0f56f765ac98b97
shamim-001 <s.ahsan.ds96@gmail.com>
2023-09-13 14:03:57 +0600

    react-router-dom initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { Route, Routes } from "react-router";
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+
+import Home from "./pages/home/Home";
+import NotFound from "./pages/404/NotFound";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
